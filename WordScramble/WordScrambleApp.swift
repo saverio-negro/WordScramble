64d4a11f423f7ff776b88544f3e77c2cdeefed89
Saverio Negro <saverionegro@SaveriosMacBook.fritz.box>
2024-11-03 11:49:58 +0100

    Initial commit.

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Saverio Negro on 03/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
